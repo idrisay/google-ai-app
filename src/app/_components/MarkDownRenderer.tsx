0f8749476e12d5b1c09af08d1387aa8acf7a0957
Idris AY <97283155+idrisay@users.noreply.github.com>
2024-10-18 17:16:40 +0200

    feat: first commit

---
+++ src/app/_components/MarkDownRenderer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MarkdownRenderer = ({ content }: {content: string}) => {
+  return <ReactMarkdown>{content}</ReactMarkdown>;
+};
+
+export default MarkdownRenderer;
